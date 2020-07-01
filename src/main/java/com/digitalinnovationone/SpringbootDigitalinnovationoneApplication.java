78861e2f6cb22eb6963b5d6992f37dee6a015c16
Gilson Silva <gilsonsilvati@gmail.com>
2020-06-30 23:37:10 -0300

    first commit

---
+++ src/main/java/com/digitalinnovationone/SpringbootDigitalinnovationoneApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDigitalinnovationoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDigitalinnovationoneApplication.class, args);
+	}
+
+}
