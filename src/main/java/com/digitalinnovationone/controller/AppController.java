15bae2234500d30d00ae39accf8126403300ad68
Gilson Silva <gilsonsilvati@gmail.com>
2020-07-02 21:11:43 -0300

    Configurações com variáveis de ambientes

--- src/main/java/com/digitalinnovationone/controller/AppController.java
+++ src/main/java/com/digitalinnovationone/controller/AppController.java
@@ -2,17 +2,27 @@ package com.digitalinnovationone.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class AppController {
 
     @Value("${app.message}")
     private String appMessage;
 
-    @GetMapping("/")
+    @Value("${ENV_DB_URL:NENHUMA}")
+    private String dbEnvironmentVariable;
+
+    @GetMapping
     public String getAppMessage() {
         return appMessage;
+    }
+
+    @GetMapping("envVariable")
+    public String getEnvironmentVariable() {
+        return "A seguinte variável de ambiente foi passada: " + dbEnvironmentVariable;
     }
 
 }
